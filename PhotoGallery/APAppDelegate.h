d57c03db5e7a93e4786552e044e35fa8808e9d7b
freennnnn <freennnn@gmail.com>
2014-06-16 09:28:37 -0500

    Initial Commit

---
+++ PhotoGallery/APAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  APAppDelegate.h
+//  PhotoGallery
+//
+//  Created by alexander.pranevich on 6/16/14.
+//  Copyright (c) 2014 Alexander Pronevich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface APAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PhotoGallery/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PhotoGallery
+//
+//  Created by alexander.pranevich on 6/16/14.
+//  Copyright (c) 2014 Alexander Pronevich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "APAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([APAppDelegate class]));
+    }
+}
